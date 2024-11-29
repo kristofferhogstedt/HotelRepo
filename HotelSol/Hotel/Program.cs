41409616bdf3d085605d63526f7dedc85448f68a
maroxx <kristofferhogstedt@gmail.com>
2024-11-29 11:30:49 +0100

    Added NuGet for EntityFramework and AutoFac

--- HotelSol/Hotel/Program.cs
+++ HotelSol/Hotel/Program.cs
@@ -7,4 +7,8 @@
             Console.WriteLine("Hello, World!");
         }
     }
+
+    // Att göra:
+    // DI with AutoFac, Tim Corey https://www.youtube.com/watch?v=mCUNrRtVVWY
+    // Använd primary constructor för dependency injection
 }
---
+++ HotelSol/Hotel/src/Persistence/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.src.Persistence
+{
+    internal class DBContext
+    {
+        public DBContext()
+        {
+        }
+        public DbSet<Purchase> Purchases { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("MyDb");
+        }
+    }
+}
