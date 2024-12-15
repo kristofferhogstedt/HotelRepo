b80ff64071627a639006f03b8dbffbaf6d909650
Kristoffer Högstedt <kristofferhogstedt@gmail.com>
2024-12-15 16:40:31 +0100

    Working on factory/singleton functions

---
+++ HotelSol/HotelLibrary/Services/Factory/Factory.cs
@@ -0,0 +1,21 @@
+﻿using HotelLibrary.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.src.FactoryManagement
+{
+    internal class Factory
+    {
+        // Singleton 
+        public Factory()
+        { 
+            IInstantiatable<T> GetInstance()
+            {
+
+            }
+        }
+    }
+}
---
+++ HotelSol/HotelLibrary/Services/Instancer.cs
@@ -0,0 +1,20 @@
+﻿using HotelLibrary.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelLibrary.Services
+{
+    public static class Instancer<T>
+    {
+        static T? _instance;
+        static ICustomerService GetInstance(IInstantiatable<T> classToInstantiate)
+        {
+            if (classToInstantiate.Instance == null)
+                _instance = Factory.GetInstance();
+            return _instance;
+        }
+    }
+}
