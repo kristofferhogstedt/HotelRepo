06b93ae119d69fb2a2159ceb76ad720e1932642f
maroxx <kristofferhogstedt@gmail.com>
2024-12-16 15:50:34 +0100

    Added DbFirst functionality from lesson07

---
+++ HotelSol/Hotel/src/Persistence/DatabaseLair.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.src.Persistence
+{
+    public class DatabaseLair
+    {
+        public ApplicationDbContext GetDbContext()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json",
+                true, true);
+            var config = builder.Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            var options = new DbContextOptionsBuilder<StudentContext>();
+            options.UseSqlServer(connectionString);
+
+            return new StudentContext(options.Options);
+        }
+    }
+}
---
+++ HotelSol/HotelLibrary/Models/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using HotelLibrary.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelLibrary.Models.Controllers
+{
+    internal class CustomerController
+    {
+        public static void Create()
+        {
+
+        }
+
+        public static ICustomer ReadOne(StudentContext dbContext)
+        {
+            Console.Write("Ange katt: ");
+            return dbContext.Students.First(c => c.FirstName == Console.ReadLine());
+        }
+
+        public static List<ICustomer> ReadAll(StudentContext dbContext)
+        {
+            return dbContext.Students.ToList();
+        }
+
+        public static void Update(StudentContext dbContext)
+        {
+            Console.Clear();
+            StudentService.ReadAll(dbContext).ForEach(c => Console.WriteLine(c.DisplayString()));
+            Console.Write("Välj katt att uppdatera: ");
+            var _entityToUpdate = dbContext.Students.First(c => c.FirstName + " " + c.LastName == Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"uppdaterar ålder på: {_entityToUpdate.FirstName} {_entityToUpdate.LastName}");
+            Console.Write("Ange ny ålder: ");
+            //_entityToUpdate. = UserInputManager.UserInputInt();
+
+            Console.Clear();
+            Console.WriteLine("Uppdaterad lista: ");
+            StudentService.ReadAll(dbContext).ForEach(c => Console.WriteLine(c.DisplayString()));
+            dbContext.SaveChanges();
+        }
+        public void Delete()
+        {
+
+        }
+    }
+}
