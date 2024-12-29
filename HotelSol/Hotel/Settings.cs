c6c7ed2b52bbcb554df46292799486b5d7f24d4d
Kristoffer Högstedt <kristofferhogstedt@gmail.com>
2024-12-29 14:47:17 +0100

    Added settings for Checkin and Checkout time

--- HotelSol/Hotel/Settings.cs
+++ HotelSol/Hotel/Settings.cs
@@ -13,5 +13,7 @@ namespace Hotel
             $@"Server=.;Database={DatabaseName};Trusted_Connection=True;TrustServerCertificate=true;";
         public static string AppSettingsFileName { get; } = "appsettings.json";
         public static ushort BedSize { get; } = 20;
+        public static TimeOnly CheckInTime { get; } = new TimeOnly(14, 0);
+        public static TimeOnly CheckOutTime { get; } = new TimeOnly(12, 0);
     }
 }
