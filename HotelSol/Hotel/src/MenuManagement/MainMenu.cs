89854710b33cac964e0290d54a9950cb7058e0f7
Kristoffer Högstedt <kristofferhogstedt@gmail.com>
2024-12-30 15:19:12 +0100

    Work on MainMenu with Spectre.Console

--- HotelSol/Hotel/src/MenuManagement/MainMenu.cs
+++ HotelSol/Hotel/src/MenuManagement/MainMenu.cs
@@ -2,25 +2,55 @@
 using Hotel.src.MenuManagement.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hotel.src.MenuManager
 {
-    internal class MainMenu : Menu, IMenu, IPreviousMenu
+    public enum MenuOptions
     {
-        Dictionary<int, string> MenuSelection { get; init; }
-        IPreviousMenu _previousMenu;
+        [Description("Föregående meny")]
+        PreviousMenu,
+        [Description("Bokningshantering")]
+        BookingManagement,
+        [Description("Kundhantering")]
+        CustomerManagement,
+        [Description("Rumhantering")]
+        RoomManagement,
+        [Description("Admin")]
+        Admin,
+        [Description("Avsluta")]
+        Exit
+    }
 
-        public MainMenu(IPreviousMenu previousMenu) : base(previousMenu)
+    internal static class MainMenu //: Menu, IMenu, IPreviousMenu
+    {
+        //Dictionary<int, string> MenuSelection { get; init; }
+        //IPreviousMenu _previousMenu;
+
+        //public MainMenu(IPreviousMenu previousMenu) : base(previousMenu)
+        //{
+        //    _previousMenu = previousMenu;
+        //    MenuSelection = new Dictionary<int, string>();
+        //    MenuSelection.Add(0, "Föregående meny");
+        //    MenuSelection.Add(1, "Artikelhantering");
+        //    MenuSelection.Add(2, "Kampanjhantering");
+        //    MenuSelection.Add(9, "Avsluta");
+        //}
+
+        public static string GetDescription(this Enum enumValue)
         {
-            _previousMenu = previousMenu;
-            MenuSelection = new Dictionary<int, string>();
-            MenuSelection.Add(0, "Föregående meny");
-            MenuSelection.Add(1, "Artikelhantering");
-            MenuSelection.Add(2, "Kampanjhantering");
-            MenuSelection.Add(9, "Avsluta");
+            var field = enumValue.GetType().GetField(enumValue.ToString());
+            if (field == null)
+            {
+                return enumValue.ToString();
+            }
+
+            var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+            return attribute != null ? attribute.Description : enumValue.ToString();
         }
     }
 }
