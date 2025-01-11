2ae81cea26d74e511e4b30bd39d495f63a690621
Kristoffer Högstedt <kristofferhogstedt@gmail.com>
2025-01-11 22:52:39 +0100

    Small change to SQLQuery

--- For_Richard/SQLQuery_krhoHotel.sql
+++ For_Richard/SQLQuery_krhoHotel.sql
@@ -1,19 +1,21 @@
+-- Query displays number of rooms per RoomTypes/Size/Number of beds
+-- SELECT, WHERE, ORDER BY, GROUP BY and a subquery
+
 SELECT 
 	RoomTypeAndDetails.RoomType
 	, RoomTypeAndDetails.Size
 	, RoomTypeAndDetails.NumberOfBeds
-	, RoomTypeAndDetails.Price
 	, Count(ROOM.ID) AS NumberOfRooms
 FROM Rooms AS ROOM
 INNER JOIN
 (
-	SELECT ROOMD.RoomID, ROOMT.ID AS RoomTypeID, ROOMT.Name AS RoomType, ROOMD.Size, ROOMD.NumberOfBeds, ROOMD.Price
+	SELECT ROOMD.RoomID, ROOMT.ID AS RoomTypeID, ROOMT.Name AS RoomType, ROOMD.Size, ROOMD.NumberOfBeds
 	FROM RoomDetails AS ROOMD
 	INNER JOIN RoomTypes AS ROOMT
 	ON ROOMD.RoomTypeID=ROOMT.ID 
 	WHERE ROOMT.Name<>'Suite'
 ) AS RoomTypeAndDetails
 ON ROOM.ID=RoomTypeAndDetails.RoomID
-GROUP BY RoomTypeAndDetails.RoomTypeID, RoomTypeAndDetails.RoomType, RoomTypeAndDetails.Size, RoomTypeAndDetails.NumberOfBeds, RoomTypeAndDetails.Price
+GROUP BY RoomTypeAndDetails.RoomTypeID, RoomTypeAndDetails.RoomType, RoomTypeAndDetails.Size, RoomTypeAndDetails.NumberOfBeds
 ORDER BY RoomTypeAndDetails.RoomTypeID
 
